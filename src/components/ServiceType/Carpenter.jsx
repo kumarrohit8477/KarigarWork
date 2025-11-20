dd5da4e2d5a25ab4bc166149e17248119f8b58a9
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-11-20 20:10:36 +0530

    update in file

--- src/components/ServiceType/Carpenter.jsx
+++ src/components/ServiceType/Carpenter.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import CServices from './CSevices'
-import ServiceBox from '../AllServices/ServiceBox'
-import './Servicetype.css'
+import React from "react";
+import CServices from "./CSevices";
+import ServiceBox from "../AllServices/ServiceBox";
+import "./Servicetype.css";
+
 const Carpenter = () => {
   const Items = [
     "Bed",
@@ -14,66 +15,48 @@ const Carpenter = () => {
     "Shelves",
     "Cabinets",
   ];
+
   return (
     <>
       <ServiceBox serviceName="Carpenter" items={Items} />
 
       <div className="box2">
+        {/* BED */}
         <div id="bed-section" className="nm">Bed</div>
-        <div className='box3'>
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
+        <div className="box3">
+          {[...Array(10)].map((_, i) => (
+            <CServices
+              key={i}
+              name="Normal Bed (with side tables)"
+              rating="4.5"
+              reviews="5K"
+              price="10000"
+              duration="4 days"
+              image="src/assets/images/bed1.png"
+            />
+          ))}
         </div>
-        <div id="almirah-section" className="nm">Almirah</div>
-        <div className='box3'>
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-        </div>
+
+        {/* KITCHEN */}
         <div id="kitchen-section" className="nm">Kitchen</div>
-        <div className='box3'>
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
+        <div className="box3">
+          {[...Array(10)].map((_, i) => (
+            <CServices
+              key={i}
+              name="Modular Kitchen Setup"
+              rating="4.7"
+              reviews="2K"
+              price="15000"
+              duration="5 days"
+              image="src/assets/images/bed1.png"
+            />
+          ))}
         </div>
-        <div id="doors-section" className="nm">Doors</div>
-        <div className='box3'>
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-          <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
-        </div>
+
+        {/* Add more sections similarly */}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Carpenter;
--- src/components/ServiceType/Plumber.jsx
+++ src/components/ServiceType/Plumber.jsx
@@ -18,7 +18,7 @@ const Plumber = () => {
       <ServiceBox serviceName="Plumber" items={Items} />
 
       <div className="box2">
-        <div className="nm">Bed</div>
+        <div id="tap fitting-section" className="nm">Tap Fitting</div>
         <div className='box3'>
           <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
           <CServices name="Normal Bed (with side tables)" rating="4.5" reviews="5K" price="10000 " duration="4 days" image="src/assets/images/bed1.png" />
