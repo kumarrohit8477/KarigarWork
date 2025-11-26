b05c3d5e2050f68ff6eac0812ee9a9a8889cfbcf
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-11-26 23:59:23 +0530

    add new  file

---
+++ src/abc/AllServices/CarpenterBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ServiceBox from './ServiceBox';
+const CarpenterBox = () => {
+  const carpenterItems = [
+    "Bed",
+    "Almirah",
+    "Kitchen",
+    "Doors",
+    "Tables",
+    "Chairs",
+    "Windows",
+    "Shelves",
+    "Cabinets",
+  ];
+  return (<div style={{ display: "flex", justifyContent: "center", padding: "30px" }}>
+    <ServiceBox serviceName="Carpenter" items={carpenterItems} />
+  </div>
+  )
+}
+
+export default CarpenterBox;
---
+++ src/abc/AllServices/ServiceBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './ServiceBox.css'
+
+const ServiceBox = ({ serviceName, items }) => {
+
+  const scrollToSection = (name) => {
+    const id = name.toLowerCase() + "-section";
+    const section = document.getElementById(id);
+
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
+  return (
+    <div className="service-container">
+      <h2>{serviceName}</h2>
+
+      <div className="items-row">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="item-box"
+            onClick={() => scrollToSection(item)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ServiceBox;
