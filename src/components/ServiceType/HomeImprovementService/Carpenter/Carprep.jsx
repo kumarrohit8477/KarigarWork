00906b48ea54d0a9bba18eaf80035a1964586390
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-11-26 23:55:20 +0530

    add new  file

--- src/components/ServiceType/HomeImprovementService/Carpenter/Carprep.jsx
+++ src/components/ServiceType/HomeImprovementService/Carpenter/Carprep.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import CServices from "../../CSevices"; 
+import ServiceBox from "../../../AllServices/ServiceBox";
 
-const Carnrep = () => {
+const Carprep = () => {
+  const Items = [
+    "Bed",
+    "Almirah",
+    "Kitchen",
+    "Doors",
+    "Tables",
+    "Chairs",
+    "Windows",
+    "Shelves",
+    "Cabinets",
+  ];
+
   return (
-    <div>Carnrep</div>
-  )
-}
+    <>
+      <ServiceBox serviceName="Carpenter" items={Items} />
 
-export default Carnrep
+      <div className="box2">
+        {/* BED */}
+        <div id="bed-section" className="nm">Bed</div>
+        <div className="box3">
+          {[...Array(10)].map((_, i) => (
+            <CServices
+              key={i}
+              name="Normal Bed (with side tables)"
+              rating="4.5"
+              reviews="5K"
+              price="10000"
+              duration="4 days"
+              image="/src/assets/images/bed1.png" 
+            />
+          ))}
+        </div>
+
+        {/* KITCHEN */}
+        <div id="kitchen-section" className="nm">Kitchen</div>
+        <div className="box3">
+          {[...Array(10)].map((_, i) => (
+            <CServices
+              key={i}
+              name="Modular Kitchen Setup"
+              rating="4.7"
+              reviews="2K"
+              price="15000"
+              duration="5 days"
+              image="/src/assets/images/bed1.png"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Carprep;
